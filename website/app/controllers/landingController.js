a7f569146996db74387d56a92a507dcac996e4f6
4nc3str4l <cmuri_10@hotmail.com>
2018-05-10 03:13:43 +0200

    Basic Grid

--- website/app/controllers/landingController.js
+++ website/app/controllers/landingController.js
@@ -1,10 +1,17 @@
 (function() {
     
-    var LandingController = function ($scope, $log, Blockchain, appSettings) {
-        $scope.contractAddress = appSettings.contractAddress;
+    var LandingController = function ($scope, $log, Blockchain, appSettings, DataFactory) {
+        
+        $scope.projects = [];
+
+        function init(){
+            $scope.projects = DataFactory.getProjects();
+        }
+
+        init();
     };
     
-    LandingController.$inject = ['$scope', '$log', 'Blockchain', 'appSettings'];
+    LandingController.$inject = ['$scope', '$log', 'Blockchain', 'appSettings', 'DataFactory'];
 
     angular.module('EthStarter')
       .controller('LandingController', LandingController);
---
+++ website/app/factories/dataFactory.js
@@ -0,0 +1,83 @@
+(function() {
+    var DataFactory = function($http, $log){
+        
+        var lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+        var projects = [
+            {
+                title : 'Project 1',
+                description: lorem,
+                goal: '10',
+                raised: '1.2',
+                start_date: '10-02-2018',
+                end_date: '10-3-2018',
+            },
+            {
+                title : 'Project 2',
+                description: lorem,
+                goal: '10',
+                raised: '1.2',
+                start_date: '10-02-2018',
+                end_date: '10-3-2018',
+            },
+            {
+                title : 'Project 3',
+                description: lorem,
+                goal: '10',
+                raised: '1.2',
+                start_date: '10-02-2018',
+                end_date: '10-3-2018',
+            },
+            {
+                title : 'Project 4',
+                description: lorem,
+                goal: '10',
+                raised: '1.2',
+                start_date: '10-02-2018',
+                end_date: '10-3-2018',
+            }, 
+            {
+                title : 'Project 5',
+                description: lorem,
+                goal: '10',
+                raised: '1.2',
+                start_date: '10-02-2018',
+                end_date: '10-3-2018',
+            },
+            {
+                title : 'Project 6',
+                description: lorem,
+                goal: '10',
+                raised: '1.2',
+                start_date: '10-02-2018',
+                end_date: '10-3-2018',
+            },
+            {
+                title : 'Project 7',
+                description: lorem,
+                goal: '10',
+                raised: '1.2',
+                start_date: '10-02-2018',
+                end_date: '10-3-2018',
+            },
+            {
+                title : 'Project 8',
+                description: lorem,
+                goal: '10',
+                raised: '1.2',
+                start_date: '10-02-2018',
+                end_date: '10-3-2018',
+            }
+        ]
+
+        return{
+            getProjects : function(){
+                return projects;
+            },
+        };
+    }
+    
+    DataFactory.$inject = ['$http', '$log'];
+    angular.module('EthStarter').factory('DataFactory', DataFactory);
+
+}());
