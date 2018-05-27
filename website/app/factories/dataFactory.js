0fad99c5fa470608662ec1fb76c906a50fe019ce
gpascualg <gpascualg93@gmail.com>
2018-05-27 03:23:20 +0200

    IPFS to create campaigns

--- website/app/factories/dataFactory.js
+++ website/app/factories/dataFactory.js
@@ -20,9 +20,52 @@
                 // Done!
                 resolve(ipfs);
             });
-        });        
+        });
+
+        function validateCampaign(campaign) {
+            const expectedKeys = ['title', 'description', 'image'];
+
+            // Check all keys are present
+            for (var key of expectedKeys) {
+                if (!(key in campaign)) {
+                    console.log(key);
+                    console.log(campaign);
+                    throw new Error('Invalig campaign: missing ' + key);
+                }
+            }
+
+            // Check there are no more keys than required
+            for (key in campaign) {
+                if (expectedKeys.indexOf(key) < 0) {
+                    throw new Error('Invalig campaign: unexpected ' + key);
+                }
+            }
+        }
 
         return {
+            submitCampaign: async function(campaign) {
+                // Make sure IPFS is connected and relayed
+                await ipfsReady;
+
+                // First make sure it's valid
+                validateCampaign(campaign);
+
+                // Dump to IPFS and retrieve hash
+                var buffer = ipfs.types.Buffer(JSON.stringify(campaign));
+                var file = await ipfs.files.add(buffer);
+
+                // Obtain a uint256 representation
+                const cid = new Cids(file[0].hash).toV1();
+                var hash = cid.multihash.slice(2);
+                var str = "0x";
+                
+                for (var byte of hash) {
+                    str += byte.toString(16).padStart(2, "0");
+                }
+
+                return new web3.BigNumber(str);
+            },
+
             getCampaignById: async function(ipfsHash) {
                 // Make sure IPFS is connected and relayed
                 await ipfsReady;
@@ -31,26 +74,12 @@
                 var file = await ipfs.files.get(ipfsHash);
                 var content = file[0].content.toString('utf-8');
                 
-                // Parse JSON
-                var data = JSON.parse(content);
-                const expectedKeys = ['title', 'description', 'image'];
-
-                // Check all keys are present
-                for (var key of expectedKeys) {
-                    if (!(key in data)) {
-                        throw new Error('Invalig campaign: missing ' + key);
-                    }
-                }
-
-                // Check there are no more keys than required
-                for (key in data) {
-                    if (!(key in expectedKeys)) {
-                        throw new Error('Invalig campaign: unexpected ' + key);
-                    }
-                }
+                // Parse JSON and validate
+                var campaign = JSON.parse(content);
+                validateCampaign(campaign);
 
                 // Done!
-                return data;
+                return campaign;
             }
         };
 
